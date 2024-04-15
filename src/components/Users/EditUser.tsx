77c2f0eeee2d2e6f33c83b021500ad85b6f79098
hoang20130267 <20130267@st.hcmuaf.edu.vn>
2024-04-15 16:03:28 +0700

    update chinh sua nguoi dung

--- src/components/Users/EditUser.tsx
+++ src/components/Users/EditUser.tsx
@@ -4,19 +4,16 @@ import {
     SimpleForm,
     TextInput,
     SelectInput,
-    EditButton,
-    email,
+    email, DateInput,
 } from 'react-admin';
 import { Box, Typography } from '@mui/material';
-export const validateForm = (
-    values: Record<string, any>
-): Record<string, any> => {
+export const validateForm = (values: Record<string, any>): Record<string, any> => {
     const errors = {} as any;
     if (!values.fullName) {
         errors.fullName = 'Vui lòng nhập họ tên';
     }
     if (!values.phone) {
-        errors.phone = 'Vui lòng nhập số điện thoại';
+        errors.phoneNumber = 'Vui lòng nhập số điện thoại';
     }
     if (!values.avatar) {
         errors.avatar = 'Vui lòng nhập đường dẫn ảnh đại diện';
@@ -29,79 +26,91 @@ export const validateForm = (
             errors.email = error;
         }
     }
+    if (!values.dateOfBirth) {
+        errors.dateOfBirth = 'Vui lòng nhập đúng ngày sinh';
+    }
     return errors;
 };
-export const EditUser = () => (
-    <Edit>
-        <SimpleForm
-            sx={{ maxWidth: 500 }}
-            // Here for the GQL provider
-            defaultValues={{
-                birthday: new Date(),
-                first_seen: new Date(),
-                last_seen: new Date(),
-                has_ordered: false,
-                latest_purchase: new Date(),
-                has_newsletter: false,
-                groups: [],
-                nb_commands: 0,
-                total_spent: 0,
-            }}
-            validate={validateForm}
-        >
-            <SectionTitle label="Thông tin cá nhân" />
-            <TextInput source="id" disabled fullWidth />
-            <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
-                <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-                    <TextInput source="username" disabled isRequired fullWidth />
+export const EditUser = () => {
+    return (
+        <Edit>
+            <SimpleForm
+                sx={{ maxWidth: 500 }}
+                defaultValues={{
+                    dateOfBirth: new Date(),
+                }}
+                validate={validateForm}
+            >
+                <SectionTitle label="Thông tin cá nhân" />
+                <TextInput source="id" disabled fullWidth />
+                <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
+                    <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+                        <TextInput type="username" source="username" disabled fullWidth/>
+                    </Box>
+                    <Box flex={1} ml={{ xs: 0, sm: '0.5em' }}>
+                        <TextInput type="fullName" source="fullName" label="Họ và tên" isRequired fullWidth/>
+                    </Box>
                 </Box>
-                <Box flex={1} ml={{ xs: 0, sm: '0.5em' }}>
-                    <TextInput source="fullName" isRequired fullWidth />
+                <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
+                    <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+                        <TextInput type="phone" source="phone" label="Số điện thoại" isRequired fullWidth />
+                    </Box>
+                    <Box flex={1} ml={{ xs: 0, sm: '0.5em' }}>
+                        <TextInput type="avatar" source="avatar" label="Avatar url" isRequired fullWidth />
+                    </Box>
                 </Box>
-            </Box>
-            <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
-                <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-                    <TextInput type="phone" source="phone" isRequired fullWidth />
+                <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
+                    <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+                <TextInput type="email" source="email" isRequired fullWidth />
+                    </Box>
+                    <Box flex={1} ml={{ xs: 0, sm: '0.5em' }}>
+                <DateInput type="dateOfBirth" source="dateOfBirth" label="Ngày sinh" fullWidth/>
+                    </Box>
                 </Box>
-                <Box flex={1} ml={{ xs: 0, sm: '0.5em' }}>
-                    <TextInput type="avatar" source="avatar" isRequired fullWidth />
+                <SelectInput
+                    source="gender"
+                    label="Giới tính"
+                    choices={[
+                        { id: "male", name: 'Nam' },
+                        { id: "female", name: 'Nữ' },
+                    ]}
+                />
+                <Separator />
+                <SectionTitle label="Chức năng" />
+                <SelectInput
+                    source="role"
+                    label="Role"
+                    choices={[
+                        { id: 1, name: 'ADMIN' },
+                        { id: 2, name: 'MODERATOR' },
+                        { id: 3, name: 'USER' },
+                    ]}
+                />
+                <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
+                    <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+                        <SelectInput
+                            source="locked"
+                            label="Khóa tài khoản"
+                            choices={[
+                                { id: false, name: 'Unlocked' },
+                                { id: true, name: 'Locked' },
+                            ]}
+                        />
+                    </Box>
+                    <Box flex={1} ml={{ xs: 0, sm: '0.1em' }}>
+                        <SelectInput
+                            source="isSocial"
+                            choices={[
+                                { id: false, name: 'No' },
+                                { id: true, name: 'Yes' },
+                            ]}
+                        />
+                    </Box>
                 </Box>
-            </Box>
-            <TextInput type="email" source="email" isRequired fullWidth />
-            <Separator />
-            <SectionTitle label="Chức năng" />
-            <SelectInput
-                source="role"
-                choices={[
-                    { id: 1, name: 'ADMIN' },
-                    { id: 2, name: 'MODERATOR' },
-                    { id: 3, name: 'USER' },
-                ]}
-            />
-            <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
-                <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-                    <SelectInput
-                        source="locked"
-                        choices={[
-                            { id: false, name: 'Unlocked' },
-                            { id: true, name: 'Locked' },
-                        ]}
-                    />
-                </Box>
-                <Box flex={1} ml={{ xs: 0, sm: '0.1em' }}>
-                    <SelectInput
-                        source="isSocial"
-                        choices={[
-                            { id: false, name: 'No' },
-                            { id: true, name: 'Yes' },
-                        ]}
-                        defaultValue={1}
-                    />
-                </Box>
-            </Box>
-        </SimpleForm>
-    </Edit>
-);
+            </SimpleForm>
+        </Edit>
+    );
+};
 const SectionTitle = ({ label }: { label: string }) => {
 
     return (
